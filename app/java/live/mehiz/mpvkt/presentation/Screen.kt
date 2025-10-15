3eb1e32e83b5d4f1b88195365a29b55e5c06962f
Sakura-404 <90583891+azxcvn@users.noreply.github.com>
2025-10-16 00:15:30 +0800

    Delete app directory

--- app/java/live/mehiz/mpvkt/presentation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package live.mehiz.mpvkt.presentation
-
-import cafe.adriel.voyager.core.screen.Screen
-import cafe.adriel.voyager.core.screen.uniqueScreenKey
-
-abstract class Screen : Screen {
-  final override val key = uniqueScreenKey
-}
